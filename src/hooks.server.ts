cc7a43a864cc852b0e0be946d2af96ae1d53bcc7
Rotour <rtourne@feelingsports.com>
2024-10-21 18:22:07 +0200

    Add: TRPC sveltekit

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,27 @@
-import { COOKEYS } from '$lib/utils/cookies.utils';
-import jwt from 'jsonwebtoken';
 import type { User } from '$auth/entities/User';
+import { COOKEYS } from '$lib/utils/cookies.utils';
 import type { Handle } from '@sveltejs/kit';
+import jwt from 'jsonwebtoken';
 
-export const handle: Handle = ({ event, resolve }) => {
+import { createContext } from '$lib/trpc/context';
+import { router } from '$lib/trpc/router';
+import { sequence } from '@sveltejs/kit/hooks';
+import { createTRPCHandle } from 'trpc-sveltekit';
+
+
+
+export const handleCookies: Handle = ({ event, resolve }) => {
 	const jwtToken = event.cookies.get(COOKEYS.JWT_TOKEN);
-
+	
 	const payload = jwt.decode(jwtToken ?? '') as User | null;
 	if (!payload) return resolve(event);
-
+	
 	event.locals.user = payload;
-
+	
 	return resolve(event);
 };
+
+export const handle: Handle = sequence(
+	handleCookies,
+	createTRPCHandle({ router, createContext }),
+) 
