af1c732949352a8c42295b1e82a355fcc9e0ef0c
Rotour <rtourne@feelingsports.com>
2024-10-23 15:47:25 +0200

    Update: mute nvm

--- infra/scripts/run-typescript.sh
+++ infra/scripts/run-typescript.sh
@@ -5,7 +5,7 @@ export NVM_DIR="/usr/local/.nvm"
 source "$NVM_DIR/nvm.sh"
 
 # Use the correct Node version
-nvm use 20.10.0
+nvm use 20.10.0 > /dev/null
 
 # Set up TypeScript environment
 export PATH="/usr/local/bin/ts-5.6.3/typescript/node_modules/.bin:$PATH"
