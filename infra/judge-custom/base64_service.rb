5e969d6ec640679b4725d41d8db53f6f3922f501
Rotour <rtourne@feelingsports.com>
2024-10-26 21:06:38 +0200

    Test: add uri decode

--- infra/judge-custom/base64_service.rb
+++ infra/judge-custom/base64_service.rb
@@ -1,4 +1,5 @@
 require 'base64'
+require 'uri'
 
 module Base64Service
   def self.encode(text)
@@ -8,6 +9,22 @@ module Base64Service
 
   def self.decode(text)
     return nil if text.nil?
+    decoded = URI.decode_www_form_component(text.to_s)
+    Base64.strict_decode64(decoded).force_encoding('UTF-8')
+  rescue ArgumentError
+    # If decoding fails, try decoding without URL decoding
     Base64.strict_decode64(text.to_s).force_encoding('UTF-8')
   end
 end
+
+# module Base64Service
+#   def self.encode(text)
+#     return nil unless text
+#     Base64.encode64(text)
+#   end
+
+#   def self.decode(text)
+#     return nil unless text
+#     Base64.decode64(text) #.force_encoding("UTF-8").encode
+#   end
+# end
