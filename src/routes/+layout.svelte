545969df2e5d7ed1390640067b20e10d3cc6226b
Rotour <rtourne@feelingsports.com>
2024-12-15 23:28:57 +0100

    Add: Version log

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { UserStore } from '$auth/stores/UserStore.svelte';
+	import { onMount } from 'svelte';
 	import '../app.css';
 	import '../fonts.css';
 	import { NotificationStack as NotificationStackStore } from '../modules/notifications/stores/NotificationStack.svelte';
@@ -7,6 +8,10 @@
 	import NotificationStack from '../modules/notifications/views/NotificationStack.svelte';
 
 	let { data, children } = $props();
+
+	onMount(() => {
+		console.debug('Version: 0.0.1');
+	})
 
 	const closeNotification = (id: string) => {
 		const notif = NotificationStackStore.stack.find((n) => n.id === id);
