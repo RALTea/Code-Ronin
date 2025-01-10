df138fa71a4417bcad0d506f20f75e3a9530ae8a
Rotour <rtourne@feelingsports.com>
2025-01-10 11:43:35 +0100

    Update(demos): Fix access

--- src/lib/trpc/middlewares/demo.middleware.ts
+++ src/lib/trpc/middlewares/demo.middleware.ts
@@ -12,16 +12,16 @@ export type DemoContent = z.infer<typeof DemoContentSchema>;
 // Allow unauthorized access to the demo campaigns only, block access to other campaigns
 export const demoMiddleware = t.middleware(async (request) => {
 	const { path, ctx, next, rawInput } = request;
-	
+
 	// Check if the campaign name is the demo campaign
 	try {
 		const { campaignName } = DemoContentSchema.parse(rawInput);
-		const campaign = await prisma.campaign.findUnique({
+		const campaign = await prisma.campaign.findFirst({
 			where: {
-				name: campaignName
+				OR: [{ name: campaignName }, { slug: campaignName }]
 			}
 		});
-		console.debug('Demo campaign', {campaignName, campaign, isDemo: campaign?.isDemo === true});
+		console.debug('Demo campaign', { campaignName, campaign, isDemo: campaign?.isDemo === true });
 		if (campaign?.isDemo === true) {
 			return next({ ctx });
 		}
