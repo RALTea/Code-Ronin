9eed8421d759aaca1243d6c027f7c00b1f687870
Rotour <rtourne@feelingsports.com>
2025-10-15 10:54:55 +0200

    Fix infra issues, switched vitest output to json

--- infra/scripts/vitest-typescript.sh
+++ infra/scripts/vitest-typescript.sh
@@ -11,4 +11,4 @@ nvm use 20.10.0 > /dev/null
 export PATH="/usr/local/bin/ts-5.6.3/typescript/node_modules/.bin:$PATH"
 
 # Run the TypeScript file
-/usr/local/bin/ts-5.6.3/typescript/node_modules/.bin/vitest run
+/usr/local/bin/ts-5.6.3/typescript/node_modules/.bin/vitest run --reporter=json
